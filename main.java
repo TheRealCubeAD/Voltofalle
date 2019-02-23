c52f0002b71185dec52b9d5c748d435ab70e1144
cupnb <noah.boeckmann@gmail.com>
2019-02-23 23:57:55 +0100

    UML geupdated
    Main implementiert

---
+++ main.java
@@ -0,0 +1,10 @@
+import Voltogame.Control;
+
+public class main {
+    private static Control gameControl;
+
+    public static void main(String [] args) {
+        gameControl = new Control();
+
+    }
+}
